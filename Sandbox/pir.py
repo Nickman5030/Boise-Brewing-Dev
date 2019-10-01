18540d468dd6b81e266602c7e5b9883656044af2
Alex Esplin <alexr.esplin@gmail.com>
2019-10-01 09:15:56 -0600

    Added a very very basic counter for customer tracking

--- Sandbox/pir.py
+++ Sandbox/pir.py
@@ -7,10 +7,12 @@ GPIO.setup(25, GPIO.IN) #PIR
 
 try:
     time.sleep(2) # to stabilize sensor
+    count = 0
     while True:
         print("No motion...")
         if GPIO.input(25):
             print("Motion Detected...")
+            count += 1
             time.sleep(3) #to avoid multiple detection
         time.sleep(0.1) #loop delay, should be less than detection delay
 
