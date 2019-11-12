dbd759b75312384e6d3d296bb45151067c5e1920
Garrett Allen <neelik.n33l@gmail.com>
2019-11-12 11:02:06 -0700

    WIP GUI integration. Added an exposed class for needed data variables.

--- Src/gui.py
+++ Src/gui.py
@@ -16,43 +16,141 @@ WINDOW_HEIGHT = 480
 WINDOW_WIDTH = 800
 
 
-def set_text(var, msg):
-    """
-        Can be used to set the text of any StringVar variable
+# Globals for use by sensor program
+class SensorData(object):
+    def __init__(self):
+        self.goal = 0
+        self.relay_state = 1  # 1 = On, 0 = Off
+        self.sensor_state = 1 # 1 = On, 0 = Off
+        self.reset = 0  # 1 = Trigger Reset, 0 = No reset
+        self.relay_duration = 10  # seconds
 
-    :param var: A StringVar object
-    :param msg: The message you want the var set to
-    :return: None
-    """
-    var.set(msg)
+    def all(self):
+        """
+            Returns a dictionary of all the current data points
+
+        :return: Dict
+        """
+        return self.__dict__
+
+    def get_goal(self):
+        """
+        :return: Current value of goal
+        """
+        return self.goal
+
+    def set_goal(self, val):
+        """
+            Updates the value in goal
+
+        :param val: Value to be assigned to goal
+        :return: None
+        """
+        self.goal = val
+
+    def get_sensor_state(self):
+        return self.sensor_state
+
+    def toggle_sensor_state(self):
+        """
+            Sets the on/off state of the sensors
+
+            1 - ON
+            0 - OFF
+
+        :return: None
+        """
+        self.sensor_state = int(not self.sensor_state)
+
+    def get_relay_state(self):
+        return self.relay_state
+
+    def toggle_relay_state(self):
+        """
+            Sets the on/off state of the sensors
+
+            1 - ON
+            0 - OFF
+
+        :return: None
+        """
+        self.relay_state = int(not self.relay_state)
+
+    def get_reset(self):
+        """
+        :return: Current value of the reset attribute
+        """
+        return self.reset
+
+    def toggle_reset(self):
+        """
+            Toggles value of reset
+
+            1 - Trigger a reset
+            0 - Continue normal operation
+
+        :return: None
+        """
+        self.reset = int(not self.reset)
+
+    def get_relay_duration(self):
+        """
+        :return: Current value of the relay duration setting
+        """
+        return self.relay_duration
+
+    def set_relay_duration(self, seconds):
+        """
+            Sets the value of relation to the provided number of seconds
+
+        :param seconds: New duration to be set
+        :return: None
+        """
+        self.relay_duration = seconds
 
 
-def toggle_on_off(btn_var, msg_var, toggle_type):
-    """
-        Sets the message displayed on the button to opposite of current state. Sets user feedback message to provided
-        text
-
-    :param btn_var: Variable that is used to display the text on the button itself
-    :param msg_var: Variable that controls messages displayed at top of GUI
-    :param toggle_type: Specifies which button is being toggled to display proper messages
-    :return: None
-    """
-    msg = ""
-    if toggle_type == "relay":
-        msg = "Relay"
-    elif toggle_type == "sensor":
-        msg = "Sensor"
-
-    current_btn_text = btn_var.get()
-    if current_btn_text == "Off":
-        btn_var.set("On")
-        msg_var.set("{} Off".format(msg))
-    else:
-        btn_var.set("Off")
-        msg_var.set("{} On".format(msg))
+# Static object that can be interacted with by the sensor program
+sensor_data = SensorData()
         
 
 if __name__ == "__main__":
+    # Functions to be used by the GUI
+    def set_text(var, msg):
+        """
+            Can be used to set the text of any StringVar variable
+
+        :param var: A StringVar object
+        :param msg: The message you want the var set to
+        :return: None
+        """
+        var.set(msg)
+
+
+    def toggle_on_off(btn_var, msg_var, toggle_type):
+        """
+            Sets the message displayed on the button to opposite of current state. Sets user feedback message to provided
+            text
+
+        :param btn_var: Variable that is used to display the text on the button itself
+        :param msg_var: Variable that controls messages displayed at top of GUI
+        :param toggle_type: Specifies which button is being toggled to display proper messages
+        :return: None
+        """
+        msg = ""
+        if toggle_type == "relay":
+            msg = "Relay"
+        elif toggle_type == "sensor":
+            msg = "Sensor"
+
+        current_btn_text = btn_var.get()
+        if current_btn_text == "Off":
+            btn_var.set("On")
+            msg_var.set("{} Off".format(msg))
+        else:
+            btn_var.set("Off")
+            msg_var.set("{} On".format(msg))
+
+
     # Data variables for use by GUI
     logo_canvas_width = 175
     logo_canvas_height = 58
