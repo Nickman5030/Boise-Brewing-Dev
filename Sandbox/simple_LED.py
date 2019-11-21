819d1a4134f6b0cea53ac87c29d5caf6acc8dd2c
Nickman5030 <nicholaschapa@u.boisestate.edu>
2019-11-21 14:31:15 -0700

    commented in each color for each GPIO

--- Sandbox/simple_LED.py
+++ Sandbox/simple_LED.py
@@ -1,7 +1,11 @@
 #Turns on Blue LED light and keeps it on
+# Pin 6 is the ground
+# GPIO 17 is RED
+# GPIO 27 is GREEN
+# GPIO 22 is BLUE
 import pigpio
 
-PIN=24
+PIN=22
 BRIGHTNESS=128
 
 pi = pigpio.pi()
