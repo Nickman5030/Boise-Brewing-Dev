d067886730a05d5a4383448d89f03b9b0307483c
gallen <gallen@basedin.com>
2019-11-29 15:43:27 -0700

    Typo fix.

--- Src/gui.py
+++ Src/gui.py
@@ -168,7 +168,7 @@ def __configure_and_set_stats_text(text_var):
 def __shutdown(root):
     # send interrupt to the Sensors
     with open(os.path.join(os.getcwd(), "data", "log.txt")) as log_file:
-        log_file.write(f"{int(check_output(['pgrep', ''-f'', 'combo_sensor.py']))}")
+        log_file.write(f"{int(check_output(['pgrep', '-f', 'combo_sensor.py']))}")
     os.kill(int(check_output(["pgrep", "-f", "combo_sensor.py"])), SIGKILL)
 
     # close the GUI
